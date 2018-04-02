23e5a28f8340e76b17a08e27ab3f91e8aba974fb
bediss <bediss.boudalia@hotmail.fr>
2018-04-02 17:48:46 +0200

    "forgot password"

--- app/src/main/java/com/lantien/bediss/wave/forgotPassword.java
+++ app/src/main/java/com/lantien/bediss/wave/forgotPassword.java
@@ -1,18 +1,59 @@
 package com.lantien.bediss.wave;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class forgotPassword extends AppCompatActivity {
+
+    private static final String TAG = "FROM FORGOT PASSWORD";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
+
+        Button submitMailForgot = findViewById(R.id.submit_mail_forgot);
+
+        submitMailForgot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendEmailForgot();
+            }
+        });
+    }
+
+    public void sendEmailForgot() {
+
+        EditText email = findViewById(R.id.inputForgotEmail);
+
+        FirebaseAuth.getInstance().sendPasswordResetEmail(email.getText().toString())
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Log.e(TAG, "Email sent.");
+                        } else {
+                            Log.e(TAG, "Email NOT sent.");
+                        }
+                    }
+                });
+    }
+
+    public void backButton(View v) {
+
+        super.onBackPressed();
     }
 
 }
