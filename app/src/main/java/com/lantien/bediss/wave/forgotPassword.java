1b6aaae30bc9d8903ef70fce5a10e520062041b0
Bediss <bediss.boudalia@gmail.com>
2018-05-13 14:40:44 +0200

    "background global attribut"

--- app/src/main/java/com/lantien/bediss/wave/forgotPassword.java
+++ app/src/main/java/com/lantien/bediss/wave/forgotPassword.java
@@ -1,5 +1,6 @@
 package com.lantien.bediss.wave;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
@@ -7,6 +8,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,45 +17,74 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 
+import java.util.regex.Pattern;
+
 public class forgotPassword extends AppCompatActivity {
 
     private static final String TAG = "FROM FORGOT PASSWORD";
+
+    private EditText email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
 
+        Toolbar toolbar = findViewById(R.id.toolbar_forgot);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
         Button submitMailForgot = findViewById(R.id.submit_mail_forgot);
 
         submitMailForgot.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                sendEmailForgot();
+
+                email = findViewById(R.id.inputForgotEmail);
+
+
+                String emailStr = email.getText().toString();
+                if(isValidEmail(emailStr)) {
+                    sendEmailForgot(emailStr);
+                } else {
+
+                    
+                    email.setError("not email");
+                }
             }
         });
     }
 
-    public void sendEmailForgot() {
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
 
-        EditText email = findViewById(R.id.inputForgotEmail);
+    private boolean isValidEmail(String email) {
+        Pattern pattern = Patterns.EMAIL_ADDRESS;
+        return pattern.matcher(email).matches();
+    }
 
-        FirebaseAuth.getInstance().sendPasswordResetEmail(email.getText().toString())
+    public void sendEmailForgot(String emailstr) {
+
+        FirebaseAuth.getInstance().sendPasswordResetEmail(emailstr)
                 .addOnCompleteListener(new OnCompleteListener<Void>() {
                     @Override
                     public void onComplete(@NonNull Task<Void> task) {
                         if (task.isSuccessful()) {
                             Log.e(TAG, "Email sent.");
+
+                            email.setError("Email sent");
                         } else {
                             Log.e(TAG, "Email NOT sent.");
+                            email.setError("Email doesnt exist");
                         }
                     }
                 });
-    }
-
-    public void backButton(View v) {
-
-        super.onBackPressed();
     }
 
 }
--- app/src/main/java/com/lantien/bediss/wave/show_profil.java
+++ app/src/main/java/com/lantien/bediss/wave/show_profil.java
@@ -30,6 +30,7 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -303,7 +304,7 @@ public class show_profil extends AppCompatActivity {
 
             if(userID.equals(receivedID)) {
 
-                Log.e(TAG, "On affiche le profil : " + receivedID);
+                Log.e(TAG, "On affiche MON profil : " + receivedID);
 
                 setuserData();
                 Button setProf = findViewById(R.id.editProfil);
